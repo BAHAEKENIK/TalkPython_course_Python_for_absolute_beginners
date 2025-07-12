5b298669d95ddfa07e024e17659bb155557d2c19
bahae kenikssi <bahaekenik@gmail.com>
2025-07-12 11:02:18 +0100

    Writing your first line

---
+++ Writing your first lines of code/practice/programe.py
@@ -0,0 +1,19 @@
+#1
+# in terminale we write :python --version
+
+#2
+# x=2
+#i compute the square and cube of x
+# print(x*2)
+# print(x*3)
+
+#3
+# name=input("What is your name ?")
+# age=int(input("What is your age ?"))
+
+#4 
+
+# import datetime
+# print(f"the date now is {datetime.datetime.now()}")
+
+#5 i visualize it in python https://pythontutor.com/visualize.html#mode=edit
